57b5ef06a0df91f7444a694f05b664e1949cf707
Mirthe Tonne <mirthe.tonne@student.ucll.be>
2025-12-15 14:37:31 +0100

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import pygame
+def create_main_surface():
+        # Initialize Pygame
+        pygame.init()
 
+        # Tuple representing width and height in pixels
+        screen_size = (1024, 768)
 
-# Initialize Pygame
-pygame.init()
+        # Create window with given size
+        pygame.display.set_mode(screen_size)
+
+        while True:
+            pass  # Busy-wait for keyboard interrupt (Ctrl+C)
