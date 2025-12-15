7f97ac2e825f92d33ad2167a274d8f5bff8b8c19
Mirthe Tonne <mirthe.tonne@student.ucll.be>
2025-12-15 15:54:30 +0100

    drawing a circle

--- app.py
+++ app.py
@@ -2,18 +2,25 @@ import pygame
 from pygame.display import flip
 
 def create_main_surface():
-        # Initialize Pygame
-        pygame.init()
+    screen_size = (1024, 768)
+    screen = pygame.display.set_mode(screen_size)
+    return screen 
 
-        # Tuple representing width and height in pixels
-        screen_size = (1024, 768)
+def render_frame(xcoordinate):
+    surface = create_main_surface()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-        # Create window with given size
-        pygame.display.set_mode(screen_size)
+        pygame.draw.circle(surface, (250, 0, 0), (xcoordinate, 100), 20)  # radius 20
+        flip()
 
-        while True:
-            pass  # Busy-wait for keyboard interrupt (Ctrl+C)
+    pygame.quit()
 
-surface = create_main_surface()
-pygame.draw.circle(surface, (250,0,0), (100,100), 20, 50)
-flip()
+def main():
+    pygame.init()
+    render_frame(100)
+
+main()
