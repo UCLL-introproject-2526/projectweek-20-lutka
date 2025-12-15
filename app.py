c8dfe336ef4863584939b46d4083956ec4b6e173
Milan Van Assche <milan.vanassche@student.ucll.be>
2025-12-15 18:21:14 +0100

    kleine aanpassingen

--- app.py
+++ app.py
@@ -1,30 +1,30 @@
-import pygame
-from pygame.display import flip
+from pygame import *
 
 def create_main_surface():
     screen_size = (1024, 768)
-    return pygame.display.set_mode(screen_size)
+    return display.set_mode(screen_size)
 
 def render_frame(surface, xcoordinate):
-    pygame.draw.circle(surface, (250, 0, 0), (xcoordinate, 100), 20)
-    flip()
+    clear_surface(surface)
+    draw.circle(surface, (250, 0, 0), (xcoordinate, 100), 20)
+    display.flip()
 
 def clear_surface(surface):
     surface.fill((0, 0, 0))
 
 def main():
     x = 1
-    pygame.init()
+    init()
     surface = create_main_surface()
 
     running = True
     while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+        # for event in event.get():
+        #     if event.type == QUIT:
+        #         running = False
+        #we voegen nog geen events toe aan de event queue dus ik heb dit even weggecomment tot we het gebruiken
 
         render_frame(surface, x)
         x += 1
-        clear_surface(surface)
 
 main()
