34f964d42ea7c0eab6055d3a79c33c464b7361fc
Mirthe Tonne <mirthe.tonne@student.ucll.be>
2025-12-16 13:41:18 +0100

    moving background

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ game_display = pygame.display.set_mode((window_width, window_height))
 bg_image = pygame.image.load('hi.png')
 
 class Map():
-    def __init__(self):
+    def __init__(self, player):
         self.Image = pygame.image.load("hi.png").convert()
+        self.player = player
 
     def draw(self, game_display):
         window_size = game_display.get_size()
@@ -23,11 +24,15 @@ class Map():
 class Player: 
     def __init__(self, x, y):
         time_passed = 0
+        self.Image = pygame.image.load("sub.png").convert()
+        self.x = x
+        self.y = y
+        """
         map_size = self.Image.get_size()
         self.x += (key[K_RIGHT] - key[K_LEFT]) * 500 * time_passed
         self.y += (key[K_DOWN] - key[K_UP]) * 500 * time_passed
         self.x = max(0, min(map_size[0]-20, self.x))
-        self.y = max(0, min(map_size[1]-20, self.y))
+        self.y = max(0, min(map_size[1]-20, self.y))"""
 
     def draw(self, game_display, map_size):
         window_size = game_display.get_size()
@@ -39,7 +44,7 @@ class Player:
                 pos[i] = center[i]
             elif pos[1] > map_size[i] - center[i]:
                  pos[i] = window_size[i] - map_size[i] + pos[i]
-        game_display.blit(bg_image, (int(pos[0]), int(pos[1])))
+        game_display.blit(self.Image, (int(pos[0]), int(pos[1])))
 
 
 class State:
@@ -47,25 +52,30 @@ class State:
         self.xcoordinate = 0
         self.ycoordinate = 0
 
-    def update(self):
+    def update(self, player):
         pressed = key.get_pressed()
 
         if pressed[K_LEFT]:
             self.xcoordinate -= 2
+            player.x -= 2
         if pressed[K_RIGHT]:
             self.xcoordinate += 2
+            player.x += 2
         if pressed[K_UP]:
             self.ycoordinate -= 2
+            player.y -= 2
         if pressed[K_DOWN]:
             self.ycoordinate += 2
+            player.y += 2
 
     def render(self, surface):
-        draw.circle(
+        """draw.circle(
             surface,
             (250, 0, 0),
             (self.xcoordinate, self.ycoordinate),
             20
-        )
+        )"""
+        display.update()
         display.flip()
 
 
@@ -83,6 +93,10 @@ def main():
     surface = create_main_surface()
     state = State()
 
+    p = Player(50,50)
+
+    m = Map(p)
+    
     running = True
     while running:
         for e in event.get():
@@ -90,11 +104,12 @@ def main():
                 running = False
 
         clear_surface(surface)
-        state.update()
-        state.render(surface)
+        m.draw(game_display)
 
-        Map.draw(game_display)
-        Player.draw(game_display, map.Image.get_size())
+        p.draw(game_display, m.Image.get_size())
+        state.update(p)
+
+        state.render(surface)
 
     # time_passed = clock.tick() / 1000
 
