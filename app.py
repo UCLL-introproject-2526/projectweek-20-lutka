6f7580da966f357b8f471e65fc826f4552abe6db
aabwoo <r1101560@ucll.be>
2025-12-16 10:39:54 +0100

    uh background static blauw en dat de player circle er boven is in plaats van onder aha

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from pygame import *
+import pygame
 
-#klasse die de huidige staat van een object in de gamewereld bijhoudt
+pygame.init()
+window_width = 1024
+window_height = 768
+game_display = pygame.display.set_mode((window_width, window_height))
+
+bg_image = pygame.image.load('BASECOLOURbg.png')
+
+
 class State:
     def __init__(self):
         self.xcoordinate = 0
@@ -17,8 +25,7 @@ class State:
             self.ycoordinate -= 2
         if pressed[K_DOWN]:
             self.ycoordinate += 2
-            
-    #maakt een nieuwe frame door over de oude te tekenen
+
     def render(self, surface):
         draw.circle(
             surface,
@@ -26,39 +33,32 @@ class State:
             (self.xcoordinate, self.ycoordinate),
             20
         )
-        #flip kopieert de backbuffer naar de frontbuffer zie "Drawing a Circle"
         display.flip()
 
-#maakt het window waarin we dingen kunnen afbeelden
+
 def create_main_surface():
     screen_size = (1024, 768)
     return display.set_mode(screen_size)
 
-#maakt het scherm terug helemaal zwart
+
 def clear_surface(surface):
-    surface.fill((0, 0, 0))
+    surface.blit(bg_image, (0, 0))
 
-#mainfunctie die al de rest zal oproepen
+
 def main():
-    #initializeert al de pygame modules
     init()
-
     surface = create_main_surface()
-
     state = State()
 
     running = True
     while running:
-
-        #als er een event op de queue van type QUIT is, dan gaan we stoppen met deze whilelus
         for e in event.get():
             if e.type == QUIT:
                 running = False
-            time.wait(60)
 
         state.update()
-        
         clear_surface(surface)
         state.render(surface)
+
 
 main()
