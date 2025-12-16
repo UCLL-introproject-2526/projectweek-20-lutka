a06bd8dc8174b3ca35e70d5687d80daf5b42c654
Frauke De Roovere <frauke.deroovere@student.ucll.be>
2025-12-16 13:35:04 +0100

    hallo

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from pygame import *
 
-#klasse die de huidige staat van een object in de gamewereld bijhoudt
+
+# klasse die de huidige staat van een object in de gamewereld bijhoudt
 class State:
     def __init__(self):
         self.xcoordinate = 0
@@ -17,30 +18,28 @@ class State:
             self.ycoordinate -= 2
         if pressed[K_DOWN]:
             self.ycoordinate += 2
-            
-    #maakt een nieuwe frame door over de oude te tekenen
+
+    # maakt een nieuwe frame door over de oude te tekenen
     def render(self, surface):
-        draw.circle(
-            surface,
-            (250, 0, 0),
-            (self.xcoordinate, self.ycoordinate),
-            20
-        )
-        #flip kopieert de backbuffer naar de frontbuffer zie "Drawing a Circle"
+        draw.circle(surface, (250, 0, 0), (self.xcoordinate, self.ycoordinate), 20)
+        # flip kopieert de backbuffer naar de frontbuffer zie "Drawing a Circle"
         display.flip()
 
-#maakt het window waarin we dingen kunnen afbeelden
+
+# maakt het window waarin we dingen kunnen afbeelden
 def create_main_surface():
     screen_size = (1024, 768)
     return display.set_mode(screen_size)
 
-#maakt het scherm terug helemaal zwart
+
+# maakt het scherm terug helemaal zwart
 def clear_surface(surface):
     surface.fill((0, 0, 0))
 
-#mainfunctie die al de rest zal oproepen
+
+# mainfunctie die al de rest zal oproepen
 def main():
-    #initializeert al de pygame modules
+    # initializeert al de pygame modules
     init()
 
     surface = create_main_surface()
@@ -50,15 +49,16 @@ def main():
     running = True
     while running:
 
-        #als er een event op de queue van type QUIT is, dan gaan we stoppen met deze whilelus
+        # als er een event op de queue van type QUIT is, dan gaan we stoppen met deze whilelus
         for e in event.get():
             if e.type == QUIT:
                 running = False
             time.wait(60)
 
         state.update()
-        
+
         clear_surface(surface)
         state.render(surface)
+
 
 main()
