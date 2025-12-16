f9cb1855495f2e97e25f0a38983d91075ee498fd
Milan Van Assche <milan.vanassche@student.ucll.be>
2025-12-16 09:25:53 +0100

    gamestate aparte file

--- app.py
+++ app.py
@@ -1,33 +1,5 @@
 from pygame import *
-
-#klasse die de huidige staat van een object in de gamewereld bijhoudt
-class State:
-    def __init__(self):
-        self.xcoordinate = 0
-        self.ycoordinate = 0
-
-    def update(self):
-        pressed = key.get_pressed()
-
-        if pressed[K_LEFT]:
-            self.xcoordinate -= 2
-        if pressed[K_RIGHT]:
-            self.xcoordinate += 2
-        if pressed[K_UP]:
-            self.ycoordinate -= 2
-        if pressed[K_DOWN]:
-            self.ycoordinate += 2
-            
-    #maakt een nieuwe frame door over de oude te tekenen
-    def render(self, surface):
-        draw.circle(
-            surface,
-            (250, 0, 0),
-            (self.xcoordinate, self.ycoordinate),
-            20
-        )
-        #flip kopieert de backbuffer naar de frontbuffer zie "Drawing a Circle"
-        display.flip()
+from gamestate import State
 
 #maakt het window waarin we dingen kunnen afbeelden
 def create_main_surface():
@@ -54,7 +26,6 @@ def main():
         for e in event.get():
             if e.type == QUIT:
                 running = False
-            time.wait(60)
 
         state.update()
         
---
+++ gamestate.py
@@ -0,0 +1,30 @@
+from pygame import *
+
+#klasse die de huidige staat van een object in de gamewereld bijhoudt
+class State:
+    def __init__(self):
+        self.xcoordinate = 0
+        self.ycoordinate = 0
+
+    def update(self):
+        pressed = key.get_pressed()
+
+        if pressed[K_LEFT]:
+            self.xcoordinate -= 2
+        if pressed[K_RIGHT]:
+            self.xcoordinate += 2
+        if pressed[K_UP]:
+            self.ycoordinate -= 2
+        if pressed[K_DOWN]:
+            self.ycoordinate += 2
+            
+    #maakt een nieuwe frame door over de oude te tekenen
+    def render(self, surface):
+        draw.circle(
+            surface,
+            (250, 0, 0),
+            (self.xcoordinate, self.ycoordinate),
+            20
+        )
+        #flip kopieert de backbuffer naar de frontbuffer zie "Drawing a Circle"
+        display.flip()
