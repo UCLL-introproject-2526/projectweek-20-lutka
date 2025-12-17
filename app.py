e2b90107677fdae61e0ad9794cf2927e603fff5c
Mirthe Tonne <mirthe.tonne@student.ucll.be>
2025-12-17 09:41:56 +0100

    working oxygen bar

--- app.py
+++ app.py
@@ -1,6 +1,39 @@
 from pygame import *
 
 
+class Timer:
+    def __init__(self, max_time):
+        self.max_time = max_time
+        self.time_left = max_time
+        self.TIMER_EVENT = USEREVENT + 1
+        time.set_timer(self.TIMER_EVENT, 1000)
+
+        self.bar_x = 680
+        self.bar_y = 700
+        self.bar_width = 300
+        self.bar_height = 20
+
+    def handle_event(self, event):
+        if event.type == self.TIMER_EVENT:
+            if self.time_left > 0:
+                self.time_left -= 1
+            else:
+                time.set_timer(self.TIMER_EVENT, 0)
+
+    def render(self, surface):
+        draw.rect(surface, (100, 100, 100),
+                  (self.bar_x, self.bar_y,
+                   self.bar_width, self.bar_height))
+
+        current_width = int(
+            (self.time_left / self.max_time) * self.bar_width
+        )
+
+        draw.rect(surface, (117, 171, 217),
+                  (self.bar_x, self.bar_y,
+                   current_width, self.bar_height))
+
+
 # klasse die de huidige staat van een object in de gamewereld bijhoudt
 class State:
     def __init__(self):
@@ -21,9 +54,8 @@ class State:
 
     # maakt een nieuwe frame door over de oude te tekenen
     def render(self, surface):
-        draw.circle(surface, (250, 0, 0), (self.xcoordinate, self.ycoordinate), 20)
-        # flip kopieert de backbuffer naar de frontbuffer zie "Drawing a Circle"
-        display.flip()
+        draw.circle(surface, (250, 0, 0),
+                    (self.xcoordinate, self.ycoordinate), 20)
 
 
 # maakt het window waarin we dingen kunnen afbeelden
@@ -41,26 +73,33 @@ def clear_surface(surface):
 def main():
     # initializeert al de pygame modules
     init()
-    mixer.music.load("christmas-jazz-christmas-holiday-347485.mp3")
-    mixer.music.play(-1)
+    # mixer.music.load("christmas-jazz-christmas-holiday-347485.mp3")
+    # mixer.music.play(-1)
 
     surface = create_main_surface()
 
     state = State()
+    timer = Timer(60)
 
     running = True
+    clock = time.Clock()
+
     while running:
 
         # als er een event op de queue van type QUIT is, dan gaan we stoppen met deze whilelus
         for e in event.get():
             if e.type == QUIT:
                 running = False
-            time.wait(60)
+            timer.handle_event(e)
 
         state.update()
 
         clear_surface(surface)
         state.render(surface)
+        timer.render(surface)
+
+        display.flip()
+        clock.tick(60)
+
 
 main()
-
